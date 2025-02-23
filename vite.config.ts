544e159dfbf13547f5793c166cc416c7862e98ab
Jakub Barszcz <jakub.barszcz33@gmail.com>
2025-02-23 22:28:37 +0100

    add new fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { fileURLToPath, URL } from 'node:url';
+import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath } from 'url';
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
